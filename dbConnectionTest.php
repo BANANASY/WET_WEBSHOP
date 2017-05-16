56cd94db005fb73f3bd1f12bc28a75d5b2bcbb3d
wi15b189 <pc7227@PVIEN-PC7227.konzern.dir>
2017-05-16 09:45:09 +0200

    tested connection

--- dbConnectionTest.php
+++ dbConnectionTest.php
@@ -10,30 +10,32 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php 
+        <?php
         //INCLUDE "./config/dbaccess.php";
-        
+
         $host = "mysqlsvr71.world4you.com";
         $user = "sql2558943";
         $password = "uxevy+j";
         $dbname = "7048141db1";
-    
-        $conn = new mysqli($host, $user, $password, $dbname);       
 
-        
+        $conn = new mysqli($host, $user, $password, $dbname);
+
+
         //$test = new DB();
-        
-        if($conn){
+
+        if ($conn) {
             echo "<br />DB connected";
-            
+
             $stmt = "SELECT * FROM adresse;";
-            $query = $conn -> query($stmt);
-            
-            while($row = $query -> fetch_object()){
-               echo $row->ort; 
-            }  
-            
-        }else{
+            $query = $conn->query($stmt);
+            if ($query) {
+                while ($row = $query->fetch_object()) {
+                    echo $row->ort;
+                }
+            } else {
+                echo "Query failed";
+            }
+        } else {
             echo "DB connection failed.";
         }
         ?>
