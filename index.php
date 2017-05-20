392c5fe42153e6aa3e32ca25bb8147b0e5e8f8f8
wi15b189 <pc7227@PVIEN-PC7227.konzern.dir>
2017-05-20 14:22:59 +0200

    test heute samstag nach laufen

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ and open the template in the editor.
     </head>    
     <body>
         <p>Paragraph Kalua</p>
+        <p>paragraph alex</p>
         <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container-fluid">
                 <!-- Brand and toggle get grouped for better mobile display -->
