3ffc085c01bea92556ee580620805c17e7c5145d
Pinkaro <schaller.alexander@hotmail.com>
2017-05-23 14:22:42 +0200

    db change in "produkte -> bild"

--- sql_scripts/7048141db1.sql
+++ sql_scripts/7048141db1.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 21. Mai 2017 um 21:42
--- Server-Version: 10.1.16-MariaDB
--- PHP-Version: 5.6.24
+-- Generation Time: May 23, 2017 at 02:21 PM
+-- Server version: 10.1.22-MariaDB
+-- PHP Version: 7.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `7048141db1`
+-- Database: `7048141db1`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `adresse`
+-- Table structure for table `adresse`
 --
 
 CREATE TABLE `adresse` (
@@ -34,7 +36,7 @@ CREATE TABLE `adresse` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `adresse`
+-- Dumping data for table `adresse`
 --
 
 INSERT INTO `adresse` (`aid`, `strasse`, `plz`, `ort`) VALUES
@@ -45,7 +47,7 @@ INSERT INTO `adresse` (`aid`, `strasse`, `plz`, `ort`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `bestellungen`
+-- Table structure for table `bestellungen`
 --
 
 CREATE TABLE `bestellungen` (
@@ -59,7 +61,7 @@ CREATE TABLE `bestellungen` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `bestellungen`
+-- Dumping data for table `bestellungen`
 --
 
 INSERT INTO `bestellungen` (`bid`, `produktid`, `pid`, `datum`, `zid`, `anzahl`, `gid`) VALUES
@@ -72,7 +74,8 @@ INSERT INTO `bestellungen` (`bid`, `produktid`, `pid`, `datum`, `zid`, `anzahl`,
 -- --------------------------------------------------------
 
 --
--- Stellvertreter-Struktur des Views `detail_rechnungen`
+-- Stand-in structure for view `detail_rechnungen`
+-- (See below for the actual view)
 --
 CREATE TABLE `detail_rechnungen` (
 `bezeichnung` varchar(50)
@@ -89,7 +92,8 @@ CREATE TABLE `detail_rechnungen` (
 -- --------------------------------------------------------
 
 --
--- Stellvertreter-Struktur des Views `detail_rechnungen_ext`
+-- Stand-in structure for view `detail_rechnungen_ext`
+-- (See below for the actual view)
 --
 CREATE TABLE `detail_rechnungen_ext` (
 `anrede` varchar(50)
@@ -109,7 +113,8 @@ CREATE TABLE `detail_rechnungen_ext` (
 -- --------------------------------------------------------
 
 --
--- Stellvertreter-Struktur des Views `gesamt_rechnungen`
+-- Stand-in structure for view `gesamt_rechnungen`
+-- (See below for the actual view)
 --
 CREATE TABLE `gesamt_rechnungen` (
 `anrede` varchar(50)
@@ -129,7 +134,7 @@ CREATE TABLE `gesamt_rechnungen` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `gutschein`
+-- Table structure for table `gutschein`
 --
 
 CREATE TABLE `gutschein` (
@@ -141,7 +146,7 @@ CREATE TABLE `gutschein` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `gutschein`
+-- Dumping data for table `gutschein`
 --
 
 INSERT INTO `gutschein` (`gid`, `wert`, `ablauf_datum`, `code`, `pid`) VALUES
@@ -150,7 +155,7 @@ INSERT INTO `gutschein` (`gid`, `wert`, `ablauf_datum`, `code`, `pid`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `kategorie`
+-- Table structure for table `kategorie`
 --
 
 CREATE TABLE `kategorie` (
@@ -159,7 +164,7 @@ CREATE TABLE `kategorie` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `kategorie`
+-- Dumping data for table `kategorie`
 --
 
 INSERT INTO `kategorie` (`kid`, `bezeichnung`) VALUES
@@ -172,7 +177,7 @@ INSERT INTO `kategorie` (`kid`, `bezeichnung`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `person`
+-- Table structure for table `person`
 --
 
 CREATE TABLE `person` (
@@ -187,7 +192,7 @@ CREATE TABLE `person` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `person`
+-- Dumping data for table `person`
 --
 
 INSERT INTO `person` (`pid`, `anrede`, `vorname`, `nachname`, `email`, `aid`, `activ`, `uid`) VALUES
@@ -200,7 +205,7 @@ INSERT INTO `person` (`pid`, `anrede`, `vorname`, `nachname`, `email`, `aid`, `a
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `produkt`
+-- Table structure for table `produkt`
 --
 
 CREATE TABLE `produkt` (
@@ -213,33 +218,33 @@ CREATE TABLE `produkt` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `produkt`
+-- Dumping data for table `produkt`
 --
 
 INSERT INTO `produkt` (`produktid`, `bezeichnung`, `bild`, `preis`, `bewertung`, `kid`) VALUES
-(1, 'Green banana', '../pictures/banana/green_banana.jpg', '1.99', 0, 1),
-(2, 'Yellow banana', '../pictures/banana/yellow_banana.jpg', '0.99', 0, 1),
-(3, 'Red banana', '../pictures/banana/red_banana.jpg', '2.99', 0, 1),
-(4, 'Diamond banana', '../pictures/banana/diamond_banana.jpg', '1599.99', 0, 1),
-(5, 'Strawberry yoghurt', '../pictures/yoghurt/strawberry_yoghurt.png', '3.99', 0, 2),
-(6, 'Apple yoghurt', '../pictures/yoghurt/apple_yoghurt.jpg', '2.99', 0, 2),
-(7, 'Banana yoghurt', '../pictures/yoghurt/_yoghurt.jpg', '1.99', 0, 2),
-(8, 'Meat yoghurt', '../pictures/yoghurt/strawberry_yoghurt.jpg', '7.99', 0, 2),
-(9, 'Pumpkin yoghurt', '../pictures/yoghurt/strawberry_yoghurt.jpg', '9.99', 0, 2),
-(10, 'Chicken egg', '../pictures/egg/chicken_egg.jpg', '0.99', 0, 3),
-(11, 'Ostrich egg', '../pictures/egg/ostrich_egg.jpg', '2.99', 0, 3),
-(12, 'Easter egg', '../pictures/egg/easter_egg.jpg', '4.99', 0, 3),
-(13, 'Black rice', '../pictures/rice/black_rice.jpg', '14.99', 0, 4),
-(14, 'White rice', '../pictures/rice/white_rice.jpg', '14.99', 0, 4),
-(15, 'Brown rice', '../pictures/rice/brown_rice.jpg', '14.99', 0, 4),
-(16, 'Banana costume', '../pictures/costumes/banana_costume.jpg', '49.99', 0, 5),
-(17, 'Egg costume', '../pictures/costumes/egg_costume.jpg', '24.99', 0, 5),
-(18, 'Minion costume, XL', '../pictures/costumes/minion_costume.jpg', '99.99', 0, 5);
+(1, 'Green banana', '../wet_webshop/pictures/banana/green_banana.jpg', '1.99', 0, 1),
+(2, 'Yellow banana', '../wet_webshop/pictures/banana/yellow_banana.jpg', '0.99', 0, 1),
+(3, 'Red banana', '../wet_webshop/pictures/banana/red_banana.jpg', '2.99', 0, 1),
+(4, 'Diamond banana', '../wet_webshop/pictures/banana/diamond_banana.jpg', '1599.99', 0, 1),
+(5, 'Strawberry yoghurt', '../wet_webshop/pictures/yoghurt/strawberry_yoghurt.png', '3.99', 0, 2),
+(6, 'Apple yoghurt', '../wet_webshop/pictures/yoghurt/apple_yoghurt.jpg', '2.99', 0, 2),
+(7, 'Banana yoghurt', '../wet_webshop/pictures/yoghurt/_yoghurt.jpg', '1.99', 0, 2),
+(8, 'Meat yoghurt', '../wet_webshop/pictures/yoghurt/strawberry_yoghurt.jpg', '7.99', 0, 2),
+(9, 'Pumpkin yoghurt', '../wet_webshop/pictures/yoghurt/strawberry_yoghurt.jpg', '9.99', 0, 2),
+(10, 'Chicken egg', '../wet_webshop/pictures/egg/chicken_egg.jpg', '0.99', 0, 3),
+(11, 'Ostrich egg', '../wet_webshop/pictures/egg/ostrich_egg.jpg', '2.99', 0, 3),
+(12, 'Easter egg', '../wet_webshop/pictures/egg/easter_egg.jpg', '4.99', 0, 3),
+(13, 'Black rice', '../wet_webshop/pictures/rice/black_rice.jpg', '14.99', 0, 4),
+(14, 'White rice', '../wet_webshop/pictures/rice/white_rice.jpg', '14.99', 0, 4),
+(15, 'Brown rice', '../wet_webshop/pictures/rice/brown_rice.jpg', '14.99', 0, 4),
+(16, 'Banana costume', '../wet_webshop/pictures/costumes/banana_costume.jpg', '49.99', 0, 5),
+(17, 'Egg costume', '../wet_webshop/pictures/costumes/egg_costume.jpg', '24.99', 0, 5),
+(18, 'Minion costume, XL', '../wet_webshop/pictures/costumes/minion_costume.jpg', '99.99', 0, 5);
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -250,7 +255,7 @@ CREATE TABLE `user` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`UID`, `username`, `password`, `role`) VALUES
@@ -261,7 +266,7 @@ INSERT INTO `user` (`UID`, `username`, `password`, `role`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `warenkorb`
+-- Table structure for table `warenkorb`
 --
 
 CREATE TABLE `warenkorb` (
@@ -273,7 +278,7 @@ CREATE TABLE `warenkorb` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `zahlungsinfo`
+-- Table structure for table `zahlungsinfo`
 --
 
 CREATE TABLE `zahlungsinfo` (
@@ -282,7 +287,7 @@ CREATE TABLE `zahlungsinfo` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `zahlungsinfo`
+-- Dumping data for table `zahlungsinfo`
 --
 
 INSERT INTO `zahlungsinfo` (`zid`, `zahlungsart`) VALUES
@@ -293,7 +298,7 @@ INSERT INTO `zahlungsinfo` (`zid`, `zahlungsart`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `zahlungsinfo_person`
+-- Table structure for table `zahlungsinfo_person`
 --
 
 CREATE TABLE `zahlungsinfo_person` (
@@ -302,7 +307,7 @@ CREATE TABLE `zahlungsinfo_person` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `zahlungsinfo_person`
+-- Dumping data for table `zahlungsinfo_person`
 --
 
 INSERT INTO `zahlungsinfo_person` (`zid`, `pid`) VALUES
@@ -311,7 +316,7 @@ INSERT INTO `zahlungsinfo_person` (`zid`, `pid`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur des Views `detail_rechnungen`
+-- Structure for view `detail_rechnungen`
 --
 DROP TABLE IF EXISTS `detail_rechnungen`;
 
@@ -320,7 +325,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- --------------------------------------------------------
 
 --
--- Struktur des Views `detail_rechnungen_ext`
+-- Structure for view `detail_rechnungen_ext`
 --
 DROP TABLE IF EXISTS `detail_rechnungen_ext`;
 
@@ -329,43 +334,43 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- --------------------------------------------------------
 
 --
--- Struktur des Views `gesamt_rechnungen`
+-- Structure for view `gesamt_rechnungen`
 --
 DROP TABLE IF EXISTS `gesamt_rechnungen`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `gesamt_rechnungen`  AS  select `detail_rechnungen_ext`.`anrede` AS `anrede`,`detail_rechnungen_ext`.`vorname` AS `vorname`,`detail_rechnungen_ext`.`nachname` AS `nachname`,`detail_rechnungen_ext`.`strasse` AS `strasse`,`detail_rechnungen_ext`.`plz` AS `plz`,`detail_rechnungen_ext`.`ort` AS `ort`,`detail_rechnungen_ext`.`zahlungsart` AS `zahlungsart`,`detail_rechnungen_ext`.`total` AS `total`,(`detail_rechnungen_ext`.`total` - coalesce(`gutschein`.`wert`,0)) AS `netto`,`detail_rechnungen_ext`.`datum` AS `datum`,`detail_rechnungen_ext`.`bid` AS `bid`,`detail_rechnungen_ext`.`pid` AS `pid` from (`detail_rechnungen_ext` left join `gutschein` on((`detail_rechnungen_ext`.`gid` = `gutschein`.`gid`))) ;
 
 --
--- Indizes der exportierten Tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indizes für die Tabelle `adresse`
+-- Indexes for table `adresse`
 --
 ALTER TABLE `adresse`
   ADD PRIMARY KEY (`aid`);
 
 --
--- Indizes für die Tabelle `bestellungen`
+-- Indexes for table `bestellungen`
 --
 ALTER TABLE `bestellungen`
   ADD PRIMARY KEY (`bid`,`produktid`,`pid`);
 
 --
--- Indizes für die Tabelle `gutschein`
+-- Indexes for table `gutschein`
 --
 ALTER TABLE `gutschein`
   ADD PRIMARY KEY (`gid`),
   ADD KEY `pid` (`pid`);
 
 --
--- Indizes für die Tabelle `kategorie`
+-- Indexes for table `kategorie`
 --
 ALTER TABLE `kategorie`
   ADD PRIMARY KEY (`kid`);
 
 --
--- Indizes für die Tabelle `person`
+-- Indexes for table `person`
 --
 ALTER TABLE `person`
   ADD PRIMARY KEY (`pid`),
@@ -373,81 +378,82 @@ ALTER TABLE `person`
   ADD KEY `aid` (`aid`);
 
 --
--- Indizes für die Tabelle `produkt`
+-- Indexes for table `produkt`
 --
 ALTER TABLE `produkt`
   ADD PRIMARY KEY (`produktid`),
   ADD KEY `kid` (`kid`);
 
 --
--- Indizes für die Tabelle `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`UID`);
 
 --
--- Indizes für die Tabelle `warenkorb`
+-- Indexes for table `warenkorb`
 --
 ALTER TABLE `warenkorb`
   ADD PRIMARY KEY (`produktid`,`pid`);
 
 --
--- Indizes für die Tabelle `zahlungsinfo`
+-- Indexes for table `zahlungsinfo`
 --
 ALTER TABLE `zahlungsinfo`
   ADD PRIMARY KEY (`zid`);
 
 --
--- Indizes für die Tabelle `zahlungsinfo_person`
+-- Indexes for table `zahlungsinfo_person`
 --
 ALTER TABLE `zahlungsinfo_person`
   ADD PRIMARY KEY (`zid`,`pid`),
   ADD KEY `pid` (`pid`);
 
 --
--- AUTO_INCREMENT für exportierte Tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT für Tabelle `adresse`
+-- AUTO_INCREMENT for table `adresse`
 --
 ALTER TABLE `adresse`
   MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
--- AUTO_INCREMENT für Tabelle `bestellungen`
+-- AUTO_INCREMENT for table `bestellungen`
 --
 ALTER TABLE `bestellungen`
   MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT für Tabelle `gutschein`
+-- AUTO_INCREMENT for table `gutschein`
 --
 ALTER TABLE `gutschein`
   MODIFY `gid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT für Tabelle `kategorie`
+-- AUTO_INCREMENT for table `kategorie`
 --
 ALTER TABLE `kategorie`
   MODIFY `kid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT für Tabelle `person`
+-- AUTO_INCREMENT for table `person`
 --
 ALTER TABLE `person`
   MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
--- AUTO_INCREMENT für Tabelle `produkt`
+-- AUTO_INCREMENT for table `produkt`
 --
 ALTER TABLE `produkt`
   MODIFY `produktid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
--- AUTO_INCREMENT für Tabelle `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `UID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
--- AUTO_INCREMENT für Tabelle `zahlungsinfo`
+-- AUTO_INCREMENT for table `zahlungsinfo`
 --
 ALTER TABLE `zahlungsinfo`
-  MODIFY `zid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `zid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
